82201994437592fc51b5e70ea8d2fbad4524d7eb
danishyarkhwork <danishyarkhwork@gmail.com>
2024-10-20 15:51:39 +0430

    fix; mouseover effect added to home page

--- wp-content/themes/baheer-group/assets/js/scripts.js
+++ wp-content/themes/baheer-group/assets/js/scripts.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("Theme loaded");
+  const buttons = document.querySelectorAll("button");
+
+  buttons.forEach((button) => {
+    button.addEventListener("mouseover", function () {
+      button.style.transform = "scale(1.05)";
+    });
+
+    button.addEventListener("mouseout", function () {
+      button.style.transform = "scale(1)";
+    });
+  });
 });
